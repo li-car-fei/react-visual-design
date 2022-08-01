677d5734976875512a11443d8ba2571dc6cd68bf
li-car-fei <1073490398@qq.com>
2022-08-01 11:44:43 +0800

    fix and fun

--- src/pages/visual-page/checked-comp.jsx
+++ src/pages/visual-page/checked-comp.jsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react'
+import { useState, useEffect } from 'react'
 import { Alert } from 'antd'
 // import * as VisualDesignComponents from 'react-visual-design-components'
 import * as VisualDesignComponents from '@/mobile_components'
@@ -7,20 +7,28 @@ import { Drop, Icon } from '@/components'
 
 import styles from './checked-comp.less'
 
-export default class SelectedComp extends PureComponent {
-  state = {
-    selectedList: [],
-    showDrop: false,
-    activeCompId: '',
-  }
+export default () => {
 
-  componentDidMount() {
-    window.addEventListener('message', e => {
-      this.setState({ ...JSON.parse(e.data) })
-    })
-  }
+  const [selectedList, setSelectedList] = useState([])
+  const [showDrop, setShowDrop] = useState(false)
+  const [activeCompId, setActiveCompId] = useState('')
 
-  handleEditItemClick = (id, comp) => {
+
+  useEffect(() => {
+    const eventListener = e => {
+      const data = JSON.parse(e.data)
+      setSelectedList(data.selectedList)
+      setShowDrop(data.showDrop)
+      setActiveCompId(data.activeCompId)
+    }
+    window.addEventListener('message', eventListener)
+
+    return () => {
+      window.removeEventListener('message', eventListener)
+    }
+  }, [])
+
+  const handleEditItemClick = (id, comp) => {
     window.parent.postMessage(
       JSON.stringify({
         func: 'handleEditItemClick',
@@ -30,7 +38,7 @@ export default class SelectedComp extends PureComponent {
     )
   }
 
-  handleOperateItem = ({ type, index }, e) => {
+  const handleOperateItem = ({ type, index }, e) => {
     e.stopPropagation()
     window.parent.postMessage(
       JSON.stringify({ func: 'handleOperateItem', params: { type, index } }),
@@ -38,70 +46,68 @@ export default class SelectedComp extends PureComponent {
     )
   }
 
-  handleDrop = (index, { name }) => {
+  const handleDrop = (index, { name }) => {
     window.parent.postMessage(JSON.stringify({ func: 'handleDrop', params: { index, name } }), '*')
   }
 
-  render() {
-    const { selectedList, showDrop, activeCompId } = this.state
-    return (
-      <>
-        {map(selectedList, ({ name, id, data }, index) => {
-          const Comp = VisualDesignComponents[name]
-          const showUp = index > 0
-          const showDown = index < selectedList.length - 1 && selectedList.length > 1
-          if (Comp) {
-            return (
-              <div
-                className={`${styles['comp-wrap']} ${id === activeCompId ? styles['comp-wrap-active'] : ''
-                  }`}
-                key={id}
-                tabIndex="0"
-              >
-                <Drop show={showDrop} handleDrop={this.handleDrop.bind(this, index)} />
-                <div className={styles['operate-wrap']}>
-                  <Icon
-                    className={styles['operate-item']}
-                    type="edit"
-                    size={24}
-                    onClick={this.handleEditItemClick.bind(this, id, Comp)}
-                  />
-                  <Icon
-                    className={styles['operate-item']}
-                    type="delete"
-                    size={24}
-                    onClick={this.handleOperateItem.bind(this, { type: 'delete', index })}
-                  />
-                  {showUp && (
-                    <Icon
-                      className={styles['operate-item']}
-                      type="up"
-                      size={24}
-                      onClick={this.handleOperateItem.bind(this, { type: 'up', index })}
-                    />
-                  )}
-                  {showDown && (
-                    <Icon
-                      className={styles['operate-item']}
-                      type="down"
-                      size={24}
-                      onClick={this.handleOperateItem.bind(this, { type: 'down', index })}
-                    />
-                  )}
-                </div>
-                <Comp data={data} />
-              </div>
-            )
-          }
+  return (
+    <>
+      {map(selectedList, ({ name, id, data }, index) => {
+        const Comp = VisualDesignComponents[name]
+        const showUp = index > 0
+        const showDown = index < selectedList.length - 1 && selectedList.length > 1
+        if (Comp) {
           return (
-            <div key={`${index}-null`} className={styles['null-comp']}>
-              该组件不存在
+            <div
+              className={`${styles['comp-wrap']} ${id === activeCompId ? styles['comp-wrap-active'] : ''
+                }`}
+              key={id}
+              tabIndex="0"
+            >
+              <Drop show={showDrop} handleDrop={(e) => { handleDrop(index, { ...e }) }} />
+              <div className={styles['operate-wrap']}>
+                <Icon
+                  className={styles['operate-item']}
+                  type="edit"
+                  size={24}
+                  onClick={() => { handleEditItemClick(id, Comp) }}
+                />
+                <Icon
+                  className={styles['operate-item']}
+                  type="delete"
+                  size={24}
+                  onClick={e => { handleOperateItem({ type: 'delete', index }, e) }}
+                />
+                {showUp && (
+                  <Icon
+                    className={styles['operate-item']}
+                    type="up"
+                    size={24}
+                    onClick={e => { handleOperateItem({ type: 'up', index }, e) }}
+                  />
+                )}
+                {showDown && (
+                  <Icon
+                    className={styles['operate-item']}
+                    type="down"
+                    size={24}
+                    onClick={e => { handleOperateItem({ type: 'down', index }, e) }}
+                  />
+                )}
+              </div>
+              <Comp data={data} />
             </div>
           )
-        })}
-        <Drop show={showDrop} handleDrop={this.handleDrop.bind(this, selectedList.length)} />
-        {isEmpty(selectedList) && <Alert message="请从左侧选择组件拖动到手机区域" type="info" />}
-      </>
-    )
-  }
+        }
+        return (
+          <div key={`${index}-null`} className={styles['null-comp']}>
+            该组件不存在
+          </div>
+        )
+      })}
+      <Drop show={showDrop} handleDrop={(e) => { handleDrop(selectedList.length, { ...e }) }} />
+      {isEmpty(selectedList) && <Alert message="请从左侧选择组件拖动到手机区域" type="info" />}
+    </>
+  )
+
 }
